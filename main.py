3e91fc297792e018778d581a510049e24a4fa7cb
sunainakhanchi <156029010+sunainakhanchi@users.noreply.github.com>
2024-02-22 21:28:08 -0500

    Add files via upload
    my project using api

--- main.py
+++ main.py
@@ -1,40 +1,16 @@
-import time
-from snake import Snake
-from turtle import Turtle ,Screen
-from practice import Food
-from scoreboard import Scoreboard
-turtle=Turtle()
-screen=Screen()
-segments=[]
-snake=Snake()
-food=Food()
-scoreboard=Scoreboard()
-screen.setup(600,600)
-screen.bgcolor("black")
-screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-game_is_on=True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
-    if snake.head.distance(food)<15:
-        food.refresh()
-        snake.extend()
-        scoreboard.score_up()
-    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
-        game_is_on=False
-        scoreboard.game_over()
-    for seg in segments:
-        if seg==snake.segments:
-            pass
-        elif snake.head.distance(seg)<10:
-            game_is_on=False
-            scoreboard.game_over()
-screen.exitonclick()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
